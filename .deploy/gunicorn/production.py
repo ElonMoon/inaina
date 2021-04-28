67d9706d7c7576166da6d199e53023379c9a8304
lhy <dev@lhy.kr>
2021-04-29 00:21:53 +0900

    update

---
+++ .deploy/gunicorn/production.py
@@ -0,0 +1,10 @@
+daemon = False
+chdir = "/srv/app"
+bind = "0.0.0.0:8000"
+# worker_class = "gevent"
+workers = 1
+threads = 1
+timeout = 60
+accesslog = "/var/log/gunicorn/access.log"
+errorlog = "/var/log/gunicorn/error.log"
+capture_output = True
