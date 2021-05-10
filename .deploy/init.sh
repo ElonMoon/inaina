eb773d3d72e22a01ba813cd73a4c75ddefe261fd
lhy <dev@lhy.kr>
2021-05-10 15:02:20 +0900

    update

--- .deploy/init.sh
+++ .deploy/init.sh
@@ -7,7 +7,7 @@ fi
 
 domains=(inaina.net www.inaina.net)
 rsa_key_size=4096
-data_path="./.deploy/certbot"
+data_path="./certbot"
 email="dev@lhy.kr" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
